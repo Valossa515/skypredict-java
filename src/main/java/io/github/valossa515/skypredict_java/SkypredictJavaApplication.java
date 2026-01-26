eb767f8ece44319283c8b152548d8acf0f8365bf
Felipe Martins <felipe.ooliveira@picpay.com>
2026-01-26 15:03:59 -0300

    Initial commit

---
+++ src/main/java/io/github/valossa515/skypredict_java/SkypredictJavaApplication.java
@@ -0,0 +1,13 @@
+package io.github.valossa515.skypredict_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkypredictJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkypredictJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/valossa515/skypredict_java/SkypredictJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.valossa515.skypredict_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkypredictJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
